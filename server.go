2da870cc8792b92b4703294a7150e43574e05c61
hsxflowers <g.dbregantin.ext@picpay.com>
2023-09-19 09:29:18 -0300

    db connection

--- server.go
+++ server.go
@@ -26,71 +26,93 @@ type Channel struct {
 	IsFreeze		bool `json:"isFreeze"`
 	IsEphemeral		bool `json:"isEphemeral"`
 	IgnoreProfanity	bool `json:"ignoreProfanity"`
+	UserIDs []int `json:"userIDs"`
 }
 
 type GroupChannels []Channel
 
 var groupChannels GroupChannels
 
-func getChannels(c echo.Context) error {
-	return c.JSON(http.StatusOK, groupChannels)
-}
-
 // func getChannels(c echo.Context) error {
-//     rows, err := db.Query("SELECT * FROM canais")
-//     if err != nil {
-//         log.Println(err)
-//         return echo.NewHTTPError(http.StatusInternalServerError)
-//     }
-//     defer rows.Close()
-
-//     var channels GroupChannels
-//     for rows.Next() {
-//         var channel Channel
-//         if err := rows.Scan(&channel.ChannelURL, &channel.ChannelName, &channel.CoverURL, &channel.CreateBy, &channel.Description, &channel.MemberCount, &channel.JoinedMembers, &channel.MaxMessage, &channel.CreateAt, &channel.IsSuper, &channel.IsPublic, &channel.IsFreeze, &channel.IsEphemeral, &channel.IgnoreProfanity); err != nil {
-//             log.Println(err)
-//             return echo.NewHTTPError(http.StatusInternalServerError)
-//         }
-//         channels = append(channels, channel)
-//     }
-
-//     return c.JSON(http.StatusOK, channels)
+// 	return c.JSON(http.StatusOK, groupChannels)
 // }
 
+func getChannels(c echo.Context) error {
+    rows, err := db.Query("SELECT * FROM channel")
+    if err != nil {
+        log.Println(err)
+        return echo.NewHTTPError(http.StatusInternalServerError)
+    }
+    defer rows.Close()
 
-func getChannel(c echo.Context)  error {
-	channelurl := c.Param("channelURL")
-	for i := range groupChannels {
-		if groupChannels[i].ChannelURL == channelurl {
-			return c.JSON(http.StatusOK, groupChannels[i])
-		}
-	}
-	return c.JSON(http.StatusBadRequest, nil)
+    var channels GroupChannels
+    for rows.Next() {
+        var channel Channel
+        if err := rows.Scan(&channel.ChannelURL, &channel.ChannelName, &channel.CoverURL, &channel.CreateBy, &channel.Description, &channel.MemberCount, &channel.JoinedMembers, &channel.MaxMessage, &channel.CreateAt, &channel.IsSuper, &channel.IsPublic, &channel.IsFreeze, &channel.IsEphemeral, &channel.IgnoreProfanity); err != nil {
+            log.Println(err)
+            return echo.NewHTTPError(http.StatusInternalServerError)
+        }
+        channels = append(channels, channel)
+    }
+
+    return c.JSON(http.StatusOK, channels)
 }
 
-// func getChannel(c echo.Context) error {
-//     channelURL := c.Param("channelURL")
-//     var channel Channel
+func getChannelsUsers(c echo.Context) error {
+    rows, err := db.Query("SELECT * FROM channel_users")
+    if err != nil {
+        log.Println(err)
+        return echo.NewHTTPError(http.StatusInternalServerError)
+    }
+    defer rows.Close()
+
+    var channels GroupChannels
+    for rows.Next() {
+        var channel Channel
+        if err := rows.Scan(&channel.ChannelURL, &channel.ChannelName, &channel.CoverURL, &channel.CreateBy, &channel.Description, &channel.MemberCount, &channel.JoinedMembers, &channel.MaxMessage, &channel.CreateAt, &channel.IsSuper, &channel.IsPublic, &channel.IsFreeze, &channel.IsEphemeral, &channel.IgnoreProfanity); err != nil {
+            log.Println(err)
+            return echo.NewHTTPError(http.StatusInternalServerError)
+        }
+        channels = append(channels, channel)
+    }
+
+    return c.JSON(http.StatusOK, channels)
+}
+
+
+// func getChannel(c echo.Context)  error {
+// 	channelurl := c.Param("channelURL")
+// 	for i := range groupChannels {
+// 		if groupChannels[i].ChannelURL == channelurl {
+// 			return c.JSON(http.StatusOK, groupChannels[i])
+// 		}
+// 	}
+// 	return c.JSON(http.StatusBadRequest, nil)
+// }
+
+func getChannel(c echo.Context) error {
+    channelURL := c.Param("channelURL")
+    var channel Channel
     
-//     err := db.QueryRow("SELECT * FROM canais WHERE channelURL = $1", channelURL).Scan(&channel.ChannelURL, &channel.ChannelName, &channel.CoverURL, &channel.CreateBy, &channel.Description, &channel.MemberCount, &channel.JoinedMembers, &channel.MaxMessage, &channel.CreateAt, &channel.IsSuper, &channel.IsPublic, &channel.IsFreeze, &channel.IsEphemeral, &channel.IgnoreProfanity)
-//     if err != nil {
-//         log.Println(err)
-//         return echo.NewHTTPError(http.StatusNotFound)
-//     }
+    err := db.QueryRow("SELECT * FROM channel WHERE channelURL = $1", channelURL).Scan(&channel.ChannelURL, &channel.ChannelName, &channel.CoverURL, &channel.CreateBy, &channel.Description, &channel.MemberCount, &channel.JoinedMembers, &channel.MaxMessage, &channel.CreateAt, &channel.IsSuper, &channel.IsPublic, &channel.IsFreeze, &channel.IsEphemeral, &channel.IgnoreProfanity)
+    if err != nil {
+        log.Println(err)
+        return echo.NewHTTPError(http.StatusNotFound)
+    }
 
-//     return c.JSON(http.StatusOK, channel)
-// }
-
-
-func postChannel(c echo.Context) error {
-	channel := Channel{}
-	err := c.Bind(&channel)
-	if err != nil {
-		return echo.NewHTTPError(http.StatusUnprocessableEntity)
-	}
-	groupChannels = append(groupChannels, channel)
-	return c.JSON(http.StatusCreated, groupChannels)
+    return c.JSON(http.StatusOK, channel)
 }
+
+
+// func postChannel(c echo.Context) error {
+// 	channel := Channel{}
+// 	err := c.Bind(&channel)
+// 	if err != nil {
+// 		return echo.NewHTTPError(http.StatusUnprocessableEntity)
+// 	}
+// 	groupChannels = append(groupChannels, channel)
+// 	return c.JSON(http.StatusCreated, groupChannels)
+// }
 
 // func postChannel(c echo.Context) error {
 //     channel := Channel{}
@@ -99,7 +121,7 @@ func postChannel(c echo.Context) error {
 //         return echo.NewHTTPError(http.StatusUnprocessableEntity)
 //     }
 
-//     _, err = db.Exec("INSERT INTO canais (channelURL, channelName, coverURL, createBy, description, memberCount, joinedMembers, maxMessage, createAt, isSuper, isPublic, isFreeze, isEphemeral, ignoreProfanity) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)",
+//     _, err = db.Exec("INSERT INTO channel (channelURL, channelName, coverURL, createBy, description, memberCount, joinedMembers, maxMessage, createAt, isSuper, isPublic, isFreeze, isEphemeral, ignoreProfanity) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)",
 //         channel.ChannelURL, channel.ChannelName, channel.CoverURL, channel.CreateBy, channel.Description, channel.MemberCount, channel.JoinedMembers, channel.MaxMessage, channel.CreateAt, channel.IsSuper, channel.IsPublic, channel.IsFreeze, channel.IsEphemeral, channel.IgnoreProfanity)
 //     if err != nil {
 //         log.Println(err)
@@ -109,16 +131,61 @@ func postChannel(c echo.Context) error {
 //     return c.JSON(http.StatusCreated, channel)
 // }
 
+func postChannel(c echo.Context) error {
+    channel := Channel{}
+    err := c.Bind(&channel)
+    if err != nil {
+        return echo.NewHTTPError(http.StatusUnprocessableEntity)
+    }
+
+    _, err = db.Exec("INSERT INTO channel (channelURL, channelName, coverURL, createBy, description, memberCount, joinedMembers, maxMessage, createAt, isSuper, isPublic, isFreeze, isEphemeral, ignoreProfanity) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)",
+        channel.ChannelURL, channel.ChannelName, channel.CoverURL, channel.CreateBy, channel.Description, channel.MemberCount, channel.JoinedMembers, channel.MaxMessage, channel.CreateAt, channel.IsSuper, channel.IsPublic, channel.IsFreeze, channel.IsEphemeral, channel.IgnoreProfanity)
+    if err != nil {
+        log.Println(err)
+        return echo.NewHTTPError(http.StatusInternalServerError)
+    }
+
+    var channelID string
+    err = db.QueryRow("SELECT channelURL FROM channel WHERE channelURL = $1", channel.ChannelURL).Scan(&channelID)
+    if err != nil {
+        log.Println(err)
+        return echo.NewHTTPError(http.StatusInternalServerError)
+    }
+
+    for _, userID := range channel.UserIDs {
+        _, err = db.Exec("INSERT INTO channel_users (channel_id, user_id) VALUES ($1, $2)", channelID, userID)
+        if err != nil {
+            log.Println(err)
+            return echo.NewHTTPError(http.StatusInternalServerError)
+        }
+    }
+
+    return c.JSON(http.StatusOK, "Channel created successfully")
+}
+
+
+
+// func deleteChannel(c echo.Context) error {
+// 	channelurl := c.Param("channelURL")
+// 	for i := range groupChannels {
+// 		if groupChannels[i].ChannelURL == channelurl {
+// 			groupChannels = append(groupChannels[:i], groupChannels[i+1:]...)
+// 			return c.JSON(http.StatusOK, groupChannels)
+// 		}
+// 	}
+// 	return c.JSON(http.StatusBadRequest, nil)
+// }
 
 func deleteChannel(c echo.Context) error {
-	channelurl := c.Param("channelURL")
-	for i := range groupChannels {
-		if groupChannels[i].ChannelURL == channelurl {
-			groupChannels = append(groupChannels[:i], groupChannels[i+1:]...)
-			return c.JSON(http.StatusOK, groupChannels)
-		}
-	}
-	return c.JSON(http.StatusBadRequest, nil)
+    channelURL := c.Param("channelURL")
+    
+    _, err := db.Exec("DELETE FROM channel WHERE channelURL = $1", channelURL)
+    if err != nil {
+        log.Println(err)
+        return echo.NewHTTPError(http.StatusInternalServerError)
+    }
+
+    return c.JSON(http.StatusOK, "Channel deleted")
 }
 
 func main() {
